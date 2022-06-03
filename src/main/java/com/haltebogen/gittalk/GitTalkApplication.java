6ba593fdca30b7797485e66d4a9948ecaae9a1df
oereo <dlstpgns0406@gmail.com>
2022-06-03 16:22:47 +0900

    chore: project base setting

---
+++ src/main/java/com/haltebogen/gittalk/GitTalkApplication.java
@@ -0,0 +1,13 @@
+package com.haltebogen.gittalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haltebogen/gittalk/GitTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haltebogen.gittalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
