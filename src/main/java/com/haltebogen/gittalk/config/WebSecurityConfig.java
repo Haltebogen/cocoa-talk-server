a035601561c35b8132c85501e762c58e7e6b2447
unanchoi <unusualc@likelion.org>
2022-06-07 20:55:05 +0900

    chore: Add Spring Security Config

---
+++ src/main/java/com/haltebogen/gittalk/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.haltebogen.gittalk.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(
+            HttpSecurity http
+            ) throws Exception {
+        http
+                .csrf().disable() //csrf
+                .exceptionHandling()// 예외처리
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll() // 권한 설정
+                ;
+    return http.build();
+    }
+
+}
