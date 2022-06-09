13ed94cc8c175488e48056d9f430dad668e7d2c8
oereo <dlstpgns0406@gmail.com>
2022-06-09 21:08:58 +0900

    feat: Response handler class 추가 및 comments 추가
    - data, message, status field 로 response field 정의

---
+++ src/main/java/com/haltebogen/gittalk/response/ResponseHandler.java
@@ -0,0 +1,19 @@
+package com.haltebogen.gittalk.response;
+
+public class ResponseHandler {
+    /**
+     * - data: json format data field
+     * - message: message sent from server to client
+     * - status: status code
+     *
+     * data format:
+     * {
+     *     "data": {
+     *          id:1
+     *          ...
+     *     }
+     *     "message": "ok"
+     *     "status": 200
+     * }
+     */
+}
