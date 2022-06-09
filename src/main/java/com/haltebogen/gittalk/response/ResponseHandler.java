34e51a0ee5b9742131940bfe8a432627d37f4419
oereo <dlstpgns0406@gmail.com>
2022-06-09 23:22:51 +0900

    feat: generateResponse method 구현
    - summary field 추가
    - ResponseEntity 반환

--- src/main/java/com/haltebogen/gittalk/response/ResponseHandler.java
+++ src/main/java/com/haltebogen/gittalk/response/ResponseHandler.java
@@ -1,5 +1,11 @@
 package com.haltebogen.gittalk.response;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResponseHandler {
     /**
      * - data: json format data field
@@ -8,12 +14,23 @@ public class ResponseHandler {
      *
      * data format:
      * {
+     *     "message": "ok"
+     *     "status": 200
+     *     summary:{}
      *     "data": {
      *          id:1
      *          ...
      *     }
-     *     "message": "ok"
-     *     "status": 200
+
      * }
      */
+    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Summary summary, Object data) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("message", message);
+        map.put("status", status.value());
+        map.put("summary", summary);
+        map.put("data", data);
+
+        return new ResponseEntity<Object>(map,status);
+    }
 }
