8892c277722ba67a80577bebb58eac45cc63d517
oereo <dlstpgns0406@gmail.com>
2022-06-17 00:15:50 +0900

    feat: response format 변경 및 주석 추가
    - 사용성에 있어서 효율적이지 못한 field 가 될 가능성으로 인해 summary field 삭제
    - example 주석 추가

--- src/main/java/com/haltebogen/gittalk/response/ResponseHandler.java
+++ src/main/java/com/haltebogen/gittalk/response/ResponseHandler.java
@@ -8,27 +8,32 @@ import java.util.Map;
 
 public class ResponseHandler {
     /**
+     * <pre>
+     * 1. Class Description
      * - data: json format data field
      * - message: message sent from server to client
      * - status: status code
      *
-     * data format:
+     * - data format:
      * {
      *     "message": "ok"
      *     "status": 200
-     *     summary:{}
      *     "data": {
      *          id:1
      *          ...
      *     }
-
      * }
+     *
+     * 2. Example
+     * - status OK: ResponseHandler.generateResponse("ok", HttpStatus.OK, result);
+     * - status Bad Request:  ResponseHandler.generateResponse("Please set a password of 8 or more characters, a combination of special symbols, numbers, and letters.", HttpStatus.BAD_REQUEST, null};
+     * </pre>
      */
-    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Summary summary, Object data) {
+
+    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object data) {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("message", message);
         map.put("status", status.value());
-        map.put("summary", summary);
         map.put("data", data);
 
         return new ResponseEntity<Object>(map,status);
