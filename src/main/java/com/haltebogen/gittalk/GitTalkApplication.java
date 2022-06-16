eaa1755048b9dbf34a6f2b856b2530426d2b00f4
oereo <dlstpgns0406@gmail.com>
2022-06-17 01:54:47 +0900

    feat: BaseAuditEntity class 추가
    - createdAt, modifiedAt, isRemoved 필드 추가
    - @EnableJpaAuditing annotation 추가

--- src/main/java/com/haltebogen/gittalk/GitTalkApplication.java
+++ src/main/java/com/haltebogen/gittalk/GitTalkApplication.java
@@ -2,12 +2,16 @@ package com.haltebogen.gittalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GitTalkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitTalkApplication.class, args);
 	}
+
+
 
 }
