9a25705ba31927c1f9872ab4e34565dbf70d2865
oereo <dlstpgns0406@gmail.com>
2022-06-17 17:59:26 +0900

    refactor: MemberService 추가
    - findUserBySearch method 구현

---
+++ src/main/java/com/haltebogen/gittalk/service/MemberService.java
@@ -0,0 +1,19 @@
+package com.haltebogen.gittalk.service;
+
+import com.haltebogen.gittalk.entity.Member;
+import com.haltebogen.gittalk.respository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Page<Member> findUserBySearch(Pageable pageable, String keyword) {
+        return memberRepository.findBySearch(keyword, pageable);
+    }
+}
