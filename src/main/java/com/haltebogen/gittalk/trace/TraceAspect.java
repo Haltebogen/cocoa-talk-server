075f0fad0f702499cfca850818c23558f117c031
oereo <dlstpgns0406@gmail.com>
2022-06-17 16:09:37 +0900

    feat: Trace 기능 추가
    - TraceAspect 추가
    - Trace annotation 추가

---
+++ src/main/java/com/haltebogen/gittalk/trace/TraceAspect.java
@@ -0,0 +1,19 @@
+package com.haltebogen.gittalk.trace;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class TraceAspect {
+
+    @Before("@annotation(com.haltebogen.gittalk.trace.Trace)")
+    public void doTrace(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        log.info("[trace] {} args={}", joinPoint.getSignature(), args);
+    }
+}
