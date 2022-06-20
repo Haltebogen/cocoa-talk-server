f4b643f10ade3d548e35ee708769e79d424f57c0
oereo <dlstpgns0406@gmail.com>
2022-06-21 00:23:53 +0900

    feat: WebSecurityConfig에 oauth 설정 추가
    - antMatchers 추가
    - oauth2Login 추가

--- src/main/java/com/haltebogen/gittalk/config/WebSecurityConfig.java
+++ src/main/java/com/haltebogen/gittalk/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.haltebogen.gittalk.config;
 
+import com.haltebogen.gittalk.oauth.BaseOauth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,18 +13,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final BaseOauth2UserService baseOauth2UserService;
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(
             HttpSecurity http
-            ) throws Exception {
+    ) throws Exception {
         http
                 .csrf().disable() //csrf
                 .exceptionHandling()// 예외처리
                 .and()
                 .authorizeRequests()
+                .antMatchers("/oauth2/**").authenticated()
                 .anyRequest().permitAll() // 권한 설정
-                ;
-    return http.build();
+                .and()
+                .oauth2Login().loginPage("/github").userInfoEndpoint().userService(baseOauth2UserService);
+        return http.build();
     }
+
+
 
 }
