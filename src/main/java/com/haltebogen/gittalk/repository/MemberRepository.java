9233a68a64bcc1ea732e58ba18adc8bcfbd8e07f
unanchoi <unusualc@likelion.org>
2022-06-22 01:29:49 +0900

    refactor: Add findByProviderId method

--- src/main/java/com/haltebogen/gittalk/repository/MemberRepository.java
+++ src/main/java/com/haltebogen/gittalk/repository/MemberRepository.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+    Optional<Member> findByProviderId(Long providerId);
 
     boolean existsByEmail(String email);
 
