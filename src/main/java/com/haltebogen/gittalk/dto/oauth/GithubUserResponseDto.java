f4e36b19e11c143425e01147861a0330151b64f3
unanchoi <unusualc@likelion.org>
2022-06-22 00:18:01 +0900

    feat: Add GitHubUserResponseDto

--- src/main/java/com/haltebogen/gittalk/dto/oauth/GithubUserResponseDto.java
+++ src/main/java/com/haltebogen/gittalk/dto/oauth/GithubUserResponseDto.java
@@ -1,2 +1,20 @@
-package com.haltebogen.gittalk.dto.oauth;public class GithubUserResponseDto {
+package com.haltebogen.gittalk.dto.oauth;
+
+import com.haltebogen.gittalk.entity.Member;
+import lombok.Data;
+@Data
+public class GithubUserResponseDto {
+
+    private Long id;
+    private String login;
+    private String avatar_url;
+    private String followers_url;
+    private String followings_url;
+    private String name;
+    private String bio;
+    private String company;
+
+    private Long followers;
+    private Long followings;
+
 }
