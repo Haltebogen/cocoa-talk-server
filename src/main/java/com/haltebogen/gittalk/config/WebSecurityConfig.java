cdbf2068fa02ee0b98651f86f20a1b6df00bfcfa
oereo <dlstpgns0406@gmail.com>
2022-06-24 08:07:19 +0900

    feat: authenticationManager 추가 및 defaultSecurityFilterChain 수정
    - login을 제외한 모든 api 에 대해서 authenticated 조건 부여
    - session 해제

--- src/main/java/com/haltebogen/gittalk/config/WebSecurityConfig.java
+++ src/main/java/com/haltebogen/gittalk/config/WebSecurityConfig.java
@@ -3,8 +3,11 @@ package com.haltebogen.gittalk.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,19 +16,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
     SecurityFilterChain defaultSecurityFilterChain(
             HttpSecurity http
     ) throws Exception {
-        http
+        return http
+                .cors().and()
                 .csrf().disable() //csrf
-                .exceptionHandling()// 예외처리
+                .exceptionHandling().and()// 예외처리
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/oauth2/**").authenticated()
-                .anyRequest().permitAll(); // 권한 설정
-        return http.build();
+                .antMatchers("/api/v1/auth/login").permitAll()
+                .antMatchers("/api/v1/").authenticated()
+                .anyRequest().authenticated().and()
+                .build(); // 권한 설정
     }
-
 
 
 }
