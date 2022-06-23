400ca2fbc6835c8d68e7a8959b0aedf30aa371ef
oereo <dlstpgns0406@gmail.com>
2022-06-23 19:29:53 +0900

    refactor: github login 시에 contructor error 문제가 있어 Builder pattern으로 수정

--- src/main/java/com/haltebogen/gittalk/dto/oauth/GithubUserResponseDto.java
+++ src/main/java/com/haltebogen/gittalk/dto/oauth/GithubUserResponseDto.java
@@ -1,10 +1,9 @@
 package com.haltebogen.gittalk.dto.oauth;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class GithubUserResponseDto {
 
     private Long id;
@@ -17,5 +16,29 @@ public class GithubUserResponseDto {
     private String company;
     private Long followers;
     private Long followings;
+
+    @Builder
+    public GithubUserResponseDto(
+            Long id,
+            String login,
+            String avatar_url,
+            String followers_url,
+            String followings_url,
+            String name,
+            String bio,
+            String company,
+            Long followers,
+            Long followings) {
+        this.id = id;
+        this.login = login;
+        this.avatar_url = avatar_url;
+        this.followers_url = followers_url;
+        this.followings_url = followings_url;
+        this.name = name;
+        this.bio = bio;
+        this.company = company;
+        this.followers = followers;
+        this.followings = followings;
+    }
 
 }
--- src/main/java/com/haltebogen/gittalk/dto/oauth/TokenDto.java
+++ src/main/java/com/haltebogen/gittalk/dto/oauth/TokenDto.java
@@ -1,12 +1,18 @@
 package com.haltebogen.gittalk.dto.oauth;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class TokenDto {
     private String access_token;
     private String token_type;
     private String scope;
+
+    @Builder
+    public TokenDto(String access_token, String token_type, String scope) {
+        this.access_token = access_token;
+        this.token_type = token_type;
+        this.scope = scope;
+    }
 }
