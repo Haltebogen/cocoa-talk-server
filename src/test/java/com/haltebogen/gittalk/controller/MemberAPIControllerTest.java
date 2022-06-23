9ac1a400d6ac83d4f2657eae72eda0214ca240c0
oereo <dlstpgns0406@gmail.com>
2022-06-24 01:00:54 +0900

    feat: test_search_member_exist_실패_BAD_REQUEST method 추가

--- src/test/java/com/haltebogen/gittalk/controller/MemberAPIControllerTest.java
+++ src/test/java/com/haltebogen/gittalk/controller/MemberAPIControllerTest.java
@@ -66,7 +66,6 @@ public class MemberAPIControllerTest {
         public void test_search_member_exist_more_page_성공() throws Exception {
             Member member1 = initInstance.createCustomMember(1L, "git-talk-admin");
             Member member2 = initInstance.createCustomMember(2L, "git-talk");
-            String response = initInstance.createPaginationResponse(2, true);
             memberRepository.save(member1);
             memberRepository.save(member2);
 
@@ -78,6 +77,19 @@ public class MemberAPIControllerTest {
                     .andExpect(jsonPath("$.data.hasNext").value(true));
 
 
+        }
+
+        @Test
+        @Transactional
+        @DisplayName("keyword 가 없을 시에 멤버 검색이 실패한다. - 결과가 있을 때")
+        public void test_search_member_exist_실패_BAD_REQUEST() throws Exception {
+            //given
+            Member member = initInstance.createDefaultMember();
+            memberRepository.save(member);
+            String keyword = "git-talk-admin";
+
+            mvc.perform(get(String.format("/member/search", keyword)))
+                    .andExpect(status().isBadRequest());
         }
     }
 }
