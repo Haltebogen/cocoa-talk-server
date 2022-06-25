526c2ec11797623b7d2e7afb016dc079af5cc4d3
oereo <dlstpgns0406@gmail.com>
2022-06-26 06:22:44 +0900

    feat: OauthController deprecated annotation 추가

--- src/main/java/com/haltebogen/gittalk/controller/OauthController.java
+++ src/main/java/com/haltebogen/gittalk/controller/OauthController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Deprecated
 @Controller
 @Slf4j
 @RequiredArgsConstructor
