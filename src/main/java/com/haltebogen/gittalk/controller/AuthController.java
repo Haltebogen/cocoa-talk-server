eb00cc7d7eda46ff2c4397b306e8898cc909206d
oereo <dlstpgns0406@gmail.com>
2022-06-26 13:30:03 +0900

    feat: login api 추가
    - response body 안에 다음의 값은 필수로 들어간다.
      - accessToken
      - refreshToken

---
+++ src/main/java/com/haltebogen/gittalk/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.haltebogen.gittalk.controller;
+
+import com.haltebogen.gittalk.dto.PaginationResponseDto;
+import com.haltebogen.gittalk.dto.member.MemberResponseDto;
+import com.haltebogen.gittalk.dto.oauth.GithubUserResponseDto;
+import com.haltebogen.gittalk.dto.oauth.JwtTokenDto;
+import com.haltebogen.gittalk.dto.oauth.TokenDto;
+import com.haltebogen.gittalk.entity.Member;
+import com.haltebogen.gittalk.response.ResponseHandler;
+import com.haltebogen.gittalk.service.MemberService;
+import com.haltebogen.gittalk.service.OAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final MemberService memberService;
+    private final OAuthService oAuthService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody TokenDto tokenDto) {
+        GithubUserResponseDto githubUserResponseDto = oAuthService.getGithubUserData(tokenDto);
+        Member member = memberService.createMember(githubUserResponseDto);
+        JwtTokenDto jwtTokenDto = oAuthService.createLoginMemberJwt(member);
+        return ResponseHandler.generateResponse("ok", HttpStatus.OK, jwtTokenDto);
+    }
+}
