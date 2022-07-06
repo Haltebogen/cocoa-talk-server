025f2cdf91a0ba38c7b8c3e80188dfd0f8cf212d
oereo <dlstpgns0406@gmail.com>
2022-07-06 17:56:42 +0900

    feat: FollowRepository에 @Repository 추가

--- src/main/java/com/haltebogen/gittalk/repository/FollowRepository.java
+++ src/main/java/com/haltebogen/gittalk/repository/FollowRepository.java
@@ -2,6 +2,8 @@ package com.haltebogen.gittalk.repository;
 
 import com.haltebogen.gittalk.entity.user.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 }
