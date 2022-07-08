8b8c6bd5a34dd19ef74611571b73a2c6b8fc8eb0
oereo <dlstpgns0406@gmail.com>
2022-07-09 01:01:34 +0900

    feat: FollowRepository에 findAllByFollower method 추가

--- src/main/java/com/haltebogen/gittalk/repository/FollowRepository.java
+++ src/main/java/com/haltebogen/gittalk/repository/FollowRepository.java
@@ -1,9 +1,13 @@
 package com.haltebogen.gittalk.repository;
 
 import com.haltebogen.gittalk.entity.user.Follow;
+import com.haltebogen.gittalk.entity.user.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    List<Follow> findAllByFollower(Member follower);
 }
