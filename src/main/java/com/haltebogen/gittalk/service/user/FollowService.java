06c5be70122ec4c4da5036df38668212e618f252
oereo <dlstpgns0406@gmail.com>
2022-07-09 01:11:59 +0900

    feat: getFollowers service logic 추가

--- src/main/java/com/haltebogen/gittalk/service/user/FollowService.java
+++ src/main/java/com/haltebogen/gittalk/service/user/FollowService.java
@@ -3,6 +3,7 @@ package com.haltebogen.gittalk.service.user;
 import com.haltebogen.gittalk.dto.member.FollowRequestDto;
 import com.haltebogen.gittalk.dto.member.FollowResponseDto;
 import com.haltebogen.gittalk.dto.member.MemberDetailResponseDto;
+import com.haltebogen.gittalk.dto.member.MemberResponseDto;
 import com.haltebogen.gittalk.entity.user.Follow;
 import com.haltebogen.gittalk.entity.user.FollowStatus;
 import com.haltebogen.gittalk.entity.user.Member;
@@ -12,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +39,12 @@ public class FollowService {
                 .following(new MemberDetailResponseDto(targetMember))
                 .followStatus(FollowStatus.PENDING)
                 .build();
+    }
+
+    @Transactional
+    public List<MemberResponseDto> getFollowers(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(IllegalArgumentException::new);
+        List<Follow> follows = followRepository.findAllByFollower(member);
+        return follows.stream().map(follow -> new MemberResponseDto(follow.getFollowing())).collect(Collectors.toList());
     }
 }
