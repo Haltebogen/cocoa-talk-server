3f707deb4beb70f2afa9b88782d5ce23d029fe2d
oereo <dlstpgns0406@gmail.com>
2022-07-12 22:40:37 +0900

    feat: Notification class를 Abstract에서 entity로 수정

--- src/main/java/com/haltebogen/gittalk/entity/notification/Notification.java
+++ src/main/java/com/haltebogen/gittalk/entity/notification/Notification.java
@@ -1,14 +1,22 @@
 package com.haltebogen.gittalk.entity.notification;
 
 import com.haltebogen.gittalk.entity.BaseAuditEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Getter
-@MappedSuperclass
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notification extends BaseAuditEntity {
 
+    @Id @GeneratedValue
+    private Long id;
     private String title;
     private String link;
     private String message;
