a32dfc49a378138007012a703d26af259eca25b4
unanchoi <unusualc@likelion.org>
2022-07-17 03:33:55 +0900

    feat: Add collections related ChatRoom

--- src/main/java/com/haltebogen/gittalk/entity/chat/ChatRoom.java
+++ src/main/java/com/haltebogen/gittalk/entity/chat/ChatRoom.java
@@ -1,2 +1,30 @@
-package com.haltebogen.gittalk.entity.chat;public class ChatRoom {
+package com.haltebogen.gittalk.entity.chat;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "rooms")
+@Data
+public class ChatRoom {
+
+    @Id
+    private String _id;
+
+    @CreatedDate
+    LocalDateTime createdAt;
+
+    @LastModifiedDate
+    LocalDateTime editedAt;
+
+    @Builder
+    public ChatRoom(LocalDateTime createdAt, LocalDateTime editedAt) {
+        this.createdAt = createdAt;
+        this.editedAt = editedAt;
+    }
 }
