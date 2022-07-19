06647c54e38b0bb05b733788f15e36a03ab984e1
oereo <dlstpgns0406@gmail.com>
2022-07-19 21:29:05 +0900

    feat: createFollowAllow service logic 추가

--- src/main/java/com/haltebogen/gittalk/service/user/FollowService.java
+++ src/main/java/com/haltebogen/gittalk/service/user/FollowService.java
@@ -25,6 +25,9 @@ public class FollowService {
     private static final String FOLLOW_REQUEST_BODY = "%s님이 친구 요청을 하였습니다.";
     private static final String FOLLOW_REQUEST_LINK = "http://localhost:3000";
 
+    private static final String FOLLOW_ALLOW_TITLE = "친구 요청 수락";
+    private static final String FOLLOW_ALLOW_BODY = "%s님이 친구 요청을 수락하였습니다.";
+    private static final String FOLLOW_ALLOW_LINK = "http://localhost:3000";
     private final FollowRepository followRepository;
     private final MemberRepository memberRepository;
     private final NotificationService notificationService;
@@ -49,7 +52,7 @@ public class FollowService {
                 .notificationType(NotificationType.FOLLOW_REQUEST)
                 .build();
 
-        notificationService.createNotification(member, notificationDto);
+        notificationService.createNotification(targetMember, notificationDto);
         return FollowResponseDto.builder()
                 .follower(new MemberDetailResponseDto(member))
                 .following(new MemberDetailResponseDto(targetMember))
@@ -57,7 +60,32 @@ public class FollowService {
                 .build();
     }
 
-    @Transactional
+    public FollowResponseDto createFollowAllow(Long memberId, Long followTargetMemberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(IllegalArgumentException::new);
+        Member targetMember = memberRepository.findById(followTargetMemberId).orElseThrow(IllegalArgumentException::new);
+        Follow follow = followRepository.findByFollowingAndFollowStatus(member, FollowStatus.PENDING);
+
+        follow.updateFollowStatus(FollowStatus.COMPLETED);
+
+        NotificationDto notificationDto = NotificationDto.builder()
+                .title(String.format(FOLLOW_ALLOW_TITLE))
+                .link(FOLLOW_ALLOW_LINK)
+                .message(String.format(FOLLOW_ALLOW_BODY, member.getNickName()))
+                .isRead(false)
+                .notificationType(NotificationType.FOLLOW_ALLOW)
+                .build();
+
+        notificationService.createNotification(targetMember, notificationDto);
+
+        return FollowResponseDto.builder()
+                .follower(new MemberDetailResponseDto(member))
+                .following(new MemberDetailResponseDto(follow.getFollowing()))
+                .followStatus(FollowStatus.COMPLETED)
+                .build();
+    }
+
+
+        @Transactional
     public List<MemberDetailResponseDto> getFollowers(Long memberId) {
         Member member = memberRepository.findById(memberId).orElseThrow(IllegalArgumentException::new);
         List<Follow> follows = followRepository.findAllByFollowerAndFollowStatus(member, FollowStatus.COMPLETED);
