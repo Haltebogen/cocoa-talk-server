24f19e378e7c381fec88956dd0b84d86a9fdb9cc
oereo <dlstpgns0406@gmail.com>
2022-07-19 20:57:13 +0900

    feat: NotificationType에 enum value 수정
    - 기존 FOLLOW 삭제
    - FOLLOW_REQUEST, FOLLOW_ALLOW 값으로 분리

--- src/main/java/com/haltebogen/gittalk/entity/notification/NotificationType.java
+++ src/main/java/com/haltebogen/gittalk/entity/notification/NotificationType.java
@@ -1,5 +1,5 @@
 package com.haltebogen.gittalk.entity.notification;
 
 public enum NotificationType {
-    FOLLOW, SERVICE_JOIN, CHAT_MESSAGE, CHAT_JOIN
+    FOLLOW_REQUEST, FOLLOW_ALLOW, SERVICE_JOIN, CHAT_MESSAGE, CHAT_JOIN
 }
