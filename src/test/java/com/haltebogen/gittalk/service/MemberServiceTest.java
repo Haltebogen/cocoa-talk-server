82a9605f8ca3843b80933796d9902cb7a116d2b7
oereo <dlstpgns0406@gmail.com>
2022-07-28 18:32:08 +0900

    feat: nested TestSearchGithubMember 추가
    - test_find_github_follow_by_search_성공 test case 추가

--- src/test/java/com/haltebogen/gittalk/service/MemberServiceTest.java
+++ src/test/java/com/haltebogen/gittalk/service/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package com.haltebogen.gittalk.service;
 
+import com.haltebogen.gittalk.dto.member.SearchGithubFollowResponseDto;
 import com.haltebogen.gittalk.dto.oauth.GithubUserResponseDto;
 import com.haltebogen.gittalk.entity.user.Member;
 import com.haltebogen.gittalk.entity.user.ProviderType;
@@ -64,6 +65,24 @@ public class MemberServiceTest {
 
             List<Member> member = memberRepository.findAllByProviderId(1234L);
             assertThat(member.size()).isEqualTo(1);
+        }
+    }
+
+    @Nested
+    @DisplayName("Github User Data를 기반으로 팔로우를 요청할 member 검색 테스트")
+    class TestSearchGithubMember {
+        @Test
+        @Transactional
+        @DisplayName("Github followings과 followers에 keyword와 같은 닉네임의 github user가 있을 경우 리스트에 조회된다.")
+        public void test_find_github_follow_by_search_성공() {
+            GithubUserResponseDto githubUserResponseDto = initMember.createGithubUserResponseDto(1234L, "oereo");
+            Member member = memberService.createMember(githubUserResponseDto);
+            String keyword = "unanchoi";
+
+            List<SearchGithubFollowResponseDto> results =  memberService.findGithubFollowBySearch(member.getId(), keyword);
+
+            assertThat(results.size()).isEqualTo(1);
+            assertThat(results.get(0).getNickName()).isEqualTo(keyword);
         }
     }
 }
