ce27c687178771e7a279d41b1c17dac82d34fa6f
unanchoi <unusualc@likelion.org>
2022-08-10 00:47:52 +0900

    feat: 채팅방 생성 서비스 로직 추가 issue에 올린 사항 해결후에 refactor 해야함.

--- src/main/java/com/haltebogen/gittalk/service/chat/ChatService.java
+++ src/main/java/com/haltebogen/gittalk/service/chat/ChatService.java
@@ -2,6 +2,9 @@ package com.haltebogen.gittalk.service.chat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.haltebogen.gittalk.dto.chat.ChatRoomRegisterDto;
+import com.haltebogen.gittalk.entity.chat.ChatMessage;
+import com.haltebogen.gittalk.entity.chat.ChatRoom;
+import com.haltebogen.gittalk.repository.chat.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,8 +15,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatService {
 
+    private final ChatRoomRepository chatRoomRepository;
+
     @Transactional
     public void createChatRoom(ChatRoomRegisterDto chatRoomRegisterDto) throws JsonProcessingException {
-        chatRoomRegisterDto.getParticipantsId();
+
+        ChatRoom chatRoom = ChatRoom.builder()
+                ._id(chatRoomRegisterDto.get_id())
+                .roomName(chatRoomRegisterDto.getRoomName())
+                .participantsId(chatRoomRegisterDto.getParticipantsId());
+
+        chatRoomRepository.save(chatRoom);
     }
+
 }
