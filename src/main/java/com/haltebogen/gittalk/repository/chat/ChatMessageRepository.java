7b6a69eb8d270622b4c776abfb60ebfbc607d671
unanchoi <unusualc@likelion.org>
2022-08-11 01:32:49 +0900

    refactor: Add @Repository annotation

--- src/main/java/com/haltebogen/gittalk/repository/chat/ChatMessageRepository.java
+++ src/main/java/com/haltebogen/gittalk/repository/chat/ChatMessageRepository.java
@@ -2,6 +2,8 @@ package com.haltebogen.gittalk.repository.chat;
 
 import com.haltebogen.gittalk.entity.chat.ChatMessage;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
 }
--- src/main/java/com/haltebogen/gittalk/repository/chat/ChatRoomRepository.java
+++ src/main/java/com/haltebogen/gittalk/repository/chat/ChatRoomRepository.java
@@ -2,6 +2,8 @@ package com.haltebogen.gittalk.repository.chat;
 
 import com.haltebogen.gittalk.entity.chat.ChatRoom;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
 }
